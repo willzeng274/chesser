3daeda9fa05d9c74227e7b4e347f72ca08a6e154
Michaelsoft Binbows <61915438+NastyPigz@users.noreply.github.com>
2024-07-03 21:21:16 -0400

    First commit

---
+++ src/components/board.tsx
@@ -0,0 +1,14 @@
+import { useEffect, useRef, useState } from 'react';
+
+export default function Board() {
+    
+
+    useEffect(() => {
+
+    }, []);
+
+    return (
+        <canvas>
+        </canvas>
+    );
+}
---
+++ src/utils/chess/bitboard.ts
@@ -0,0 +1,141 @@
+export class PieceType {
+        const wPawn = 0;
+        const wKnight = 1;
+        const wBishop = 2;
+        const wRook = 3;
+        const wQueen = 4;
+        const wKing = 5;
+      
+        const bPawn = 6;
+        const bKnight = 7;
+        const bBishop = 8;
+        const bRook = 9;
+        const bQueen = 1;,
+        const bKing = 1;,
+
+        
+        public get value() : string {
+            return [wPawn, wKnight, wBishop, wRook, wQueen, wKing],
+        }
+}
+
+export class BitBoard {
+    /*
+    BitBoard | BitBoard is union, combines all of the bits
+    BitBoard & BitBoard is interection, if both bits are 1 the result is 1 else 0
+    */
+    value: BigInt;
+    pieceType: PieceType;
+    const BitBoard(this.value, {this.pieceType});
+  
+    static const full = BitBoard(0xffffffffffffffff);
+    static const lightSquares = BitBoard(0x55AA55AA55AA55AA);
+    static const darkSquares = BitBoard(0xAA55AA55AA55AA55);
+    static const diagonal = BitBoard(0x8040201008040201);
+    static const antidiagonal = BitBoard(0x0102040810204080);
+    static const corners = BitBoard(0x8100000000000081);
+    static const center = BitBoard(0x0000001818000000);
+    static const backranks = BitBoard(0xff000000000000ff);
+  
+    BitBoard setBit(int square) {
+      assert(square >= 0 && square < 64);
+      return BitBoard(value | (1 << square));
+    }
+  
+    int getBit(int square) {
+      assert(square >= 0 && square < 64);
+      return (value >> square) & 1;
+    }
+  
+    bool has(int square) {
+      assert(square >= 0 && square < 64);
+      return getBit(square) == 1;
+    }
+  
+    BitBoard popBit(int square) {
+      assert(square >= 0 && square < 64);
+  
+      return BitBoard(value & ~(1 << (square)));
+    }
+  
+    /// Bitwise right shift
+    BitBoard shr(int shift) {
+      if (shift >= 64) return BitBoard(0);
+      if (shift > 0) return BitBoard(value >>> shift);
+      return this;
+    }
+  
+    BitBoard operator >>(int shift) => shr(shift);
+  
+    /// Bitwise left shift
+    BitBoard shl(int shift) {
+      if (shift >= 64) return BitBoard(0);
+      if (shift > 0) return BitBoard(value << shift);
+      return this;
+    }
+  
+    BitBoard operator <<(int shift) => shl(shift);
+  
+    BitBoard operator -() => BitBoard(-value);
+  
+    BitBoard xor(BitBoard other) => BitBoard(value ^ other.value);
+    BitBoard operator ^(BitBoard other) => BitBoard(value ^ other.value);
+  
+    BitBoard union(BitBoard other) => BitBoard(value | other.value);
+    BitBoard operator |(BitBoard other) => BitBoard(value | other.value);
+  
+    BitBoard intersect(BitBoard other) => BitBoard(value & other.value);
+    BitBoard operator &(BitBoard other) => BitBoard(value & other.value);
+  
+    BitBoard minus(BitBoard other) => BitBoard(value - other.value);
+    BitBoard operator -(BitBoard other) => BitBoard(value - other.value);
+  
+    BitBoard operator ~() => BitBoard(~value);
+    BitBoard complement() => BitBoard(~value);
+  
+    BitBoard operator *(int other) => BitBoard(value * other);
+  
+    BitBoard diff(BitBoard other) => BitBoard(value & ~other.value);
+  
+    @override
+    bool operator ==(Object other) {
+      return identical(this, other) ||
+          other is BitBoard &&
+              other.runtimeType == runtimeType &&
+              other.value == value;
+    }
+  
+    @override
+    int get hashCode => value.hashCode;
+  
+    @override
+    String toString() {
+      return "BitBoard($value)";
+    }
+  
+    bool get notEmpty => value != 0;
+  
+    printBoard({bool showBoardValue = true}) {
+      print('');
+  
+      for (var rank = 0; rank < 8; rank++) {
+        for (var file = 0; file < 8; file++) {
+          final square = rank * 8 + file;
+  
+          if (file == 0) {
+            stdout.write("${8 - rank}  | ");
+          }
+  
+          stdout.write('${getBit(square)} ');
+        }
+        stdout.write('\n');
+      }
+  
+      stdout.write('   ------------------');
+      stdout.write('\n     a b c d e f g h\n');
+      if (showBoardValue) {
+        print('\nBitBoard Value: $value\n');
+      }
+    }
+  }
+  
